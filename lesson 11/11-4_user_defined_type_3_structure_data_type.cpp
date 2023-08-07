b54edcb448168ddaa82c12375e2831c40a71dc9d
JinHyeok Oh <dhwlsgur795@khu.ac.kr>
2023-08-07 16:03:30 +0900

    lesson 11 in progress(2)

---
+++ lesson 11/11-4_user_defined_type_3_structure_data_type.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Car {
+	int num;
+	double gas;
+};
+
+int main() {
+	Car car1 = { 1234, 25.5 };
+	Car car2 = { 4567, 52.2 };
+
+	cout << "car1의 차량 번호는 " << car1.num << " 연료의 양은 " << car1.gas << "입니다." << endl;
+	cout << "car2의 차량 번호는 " << car2.num << " 연료의 양은 " << car2.gas << "입니다." << endl;
+
+	car2 = car1;
+
+	cout << "car1을 car2에 대입했습니다." << endl;
+
+	cout << "car2의 차량 번호는 " << car2.num << " 연료량은 " << car2.gas << "입니다." << endl;
+
+	return 0;
+}
